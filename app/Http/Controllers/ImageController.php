99b86ab5f5452b752e8980898e8c3674b302022f
chris lim <lm.chrstphr.m@gmail.com>
2018-07-09 11:53:34 +0800

    comment adjustment

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -32,11 +32,11 @@ class ImageController extends Controller
     }
 
     
-        /**
-        * Return the path to public dir
-        * @param null $path
-        * @return string
-        */
+    /**
+    * Return the path to public dir
+    * @param null $path
+    * @return string
+    */
     public function public_path($path=null){
             return rtrim(app()->basePath('public/'.$path), '/');
     }
